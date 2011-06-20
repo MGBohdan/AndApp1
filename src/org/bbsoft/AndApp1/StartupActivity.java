2f4eb2ed215f0d1d81546f3d8e2b4aabee5aabaf
MGBohdan <bb.soft@live.com>
2011-06-21 01:52:31 +0300

    create empty android project

---
+++ src/org/bbsoft/AndApp1/StartupActivity.java
@@ -0,0 +1,13 @@
+package org.bbsoft.AndApp1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StartupActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
